18d3e56041862d0fc944c3585802f5ac062a5932
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-05-02 13:20:55 +0300

    third practic version done

---
+++ app/src/main/java/ru/mirea/zhumanov/mireaproject/ui/data/DataFragment.java
@@ -0,0 +1,59 @@
+package ru.mirea.zhumanov.mireaproject.ui.data;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ru.mirea.zhumanov.mireaproject.R;
+import ru.mirea.zhumanov.mireaproject.databinding.FragmentDataBinding;
+
+public class DataFragment extends Fragment {
+
+    private FragmentDataBinding binding;
+
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DataViewModel dataViewModel =
+                new ViewModelProvider(this).get(DataViewModel.class);
+
+        binding = FragmentDataBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textData;
+        final TextView textView2 = binding.textData2;
+        textView.setText("Java — строго типизированный объектно-ориентированный язык программирования общего назначения," +
+                " разработанный компанией Sun Microsystems (в последующем приобретённой компанией Oracle)." +
+                " Разработка ведётся сообществом, организованным через Java Community Process; язык и основные" +
+                " реализующие его технологии распространяются по лицензии GPL. Права на торговую марку принадлежат" +
+                " корпорации Oracle.");
+
+        textView2.setText("Java Development Kit (сокращенно JDK) — бесплатно распространяемый компанией Oracle Corporation" +
+                " (ранее Sun Microsystems) комплект разработчика приложений на языке Java, включающий в себя компилятор Java (javac)," +
+                " стандартные библиотеки классов Java, примеры, документацию, различные утилиты и исполнительную систему Java (JRE)." +
+                " В состав JDK не входит интегрированная среда разработки на Java, поэтому разработчик, использующий только JDK," +
+                " вынужден использовать внешний текстовый редактор и компилировать свои программы, используя утилиты командной строки.");
+        //     dataViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/ru/mirea/zhumanov/mireaproject/ui/data/DataViewModel.java
@@ -0,0 +1,18 @@
+package ru.mirea.zhumanov.mireaproject.ui.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DataViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public DataViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
