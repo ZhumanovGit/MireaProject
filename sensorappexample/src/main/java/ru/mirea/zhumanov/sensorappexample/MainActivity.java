2efba757518f6519669e52381c6b250c5c6f84e7
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-05-25 20:06:24 +0300

    done for practic 5

---
+++ sensorappexample/src/main/java/ru/mirea/zhumanov/sensorappexample/MainActivity.java
@@ -0,0 +1,69 @@
+package ru.mirea.zhumanov.sensorappexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private static final int CAMERA_REQUEST = 0;
+    private boolean isWork = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int cameraPermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, android.Manifest.permission.
+                WRITE_EXTERNAL_STORAGE);
+        int audioRecordPermissionStatus = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO);
+
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus
+                == PackageManager.PERMISSION_GRANTED && audioRecordPermissionStatus
+                == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+// Выполняется запрос к пользователь на получение необходимых разрешений
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION);
+        }
+
+
+    }
+
+    public void onClickAccelerometerActivity(View view){
+        Intent intent = new Intent(this, AccelerometerActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickCameraActivity(View view){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickRecorderActivity(View view){
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
+    }
+
+
+}
